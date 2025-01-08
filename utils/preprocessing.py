0bbfb6e43729bf194bf47ec13471473f35797cd2
tanguyLC01 <tanguy.lecloirec@gmail.com>
2025-01-08 19:24:46 +0100

    Some new inputs in the models

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,6 +2,16 @@ import cv2
 import math
 import numpy as np
 
+def pad_to_square_opencv(image, target_size=(256, 256)):
+    h, w, _ = image.shape
+    desired_size = target_size[0]
+    delta_w = desired_size - w
+    delta_h = desired_size - h
+    top, bottom = delta_h // 2, delta_h - delta_h // 2
+    left, right = delta_w // 2, delta_w - delta_w // 2
+    new_image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+    return new_image
+
 def enhance_img(img):
     # CLAHE
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
@@ -22,6 +32,13 @@ def enhance_img(img):
     gamma = math.log(mid*255)/math.log(mean)
     val_gamma = np.power(val, gamma).clip(0,255).astype(np.uint8)
     hsv_gamma = cv2.merge([hue, sat, val_gamma])
-    img_gamma2 = cv2.cvtColor(hsv_gamma, cv2.COLOR_HSV2BGR)
+    img_gamma2 = cv2.cvtColor(hsv_gamma, cv2.COLOR_HSV2RGB)
     
     return img_gamma2
+
+def preprocessing_img(img):
+    if img.shape != (256, 256, 3):
+        img = pad_to_square_opencv(img)
+    img = enhance_img(img)
+    img = cv2.resize(img, (224, 224))
+    return img
